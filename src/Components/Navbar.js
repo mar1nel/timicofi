1e819713176487a2ec3c07f6c9934d712311eba4
mar1nel <vicu-marin.ababii@student.upt.ro>
2023-05-08 02:37:26 +0300

    scss resolved

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Navbar.scss';
+
+const Navbar = () => {
+    return(
+        <div className='header'>
+            <div className='container'>
+                <h1>coi<span className='primary'>NO</span></h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;

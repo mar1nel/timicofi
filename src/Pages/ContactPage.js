2906d72f00a0e7cca5d20578f484d5a9b53a619e
mar1nel <vicu-marin.ababii@student.upt.ro>
2023-05-15 20:54:11 +0300

    added contact form
    no functional

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -2,6 +2,14 @@ import React from "react";
 import { Text } from "@nextui-org/react";
 import Navbar from "../Components/Navbar";
 import "./ContactPage.scss";
+import Footer from "../Components/Footer";
+
+import { Input, Spacer } from '@nextui-org/react';
+import { Checkbox } from "@nextui-org/react";
+import { Textarea, Grid } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
+
+
 
 const ContactPage = () => {
 
@@ -30,9 +38,7 @@ const ContactPage = () => {
                                 margin: "0",
                             }}
                             weight="bold"
-                        >
-                            Team
-                        </Text>
+                        >Team</Text>
 
                         <img src="https://em-content.zobj.net/thumbs/120/apple/354/blue-heart_1f499.png" alt="/"
                              style={{ width: "60px", height: "60px", marginTop: "20px", position: "relative", left: "20px", top: "20px" }} />
@@ -47,6 +53,102 @@ const ContactPage = () => {
                     Our dedicated team is here to assist you with any questions or concerns you may have.
                 </p>
                 </div>
+
+                <div className="contact-form">
+                    <h2 style={{
+                        textAlign: "center",
+                        marginTop: "-20px",
+                        color: "white"
+                    }}>Contact Form</h2>
+                    <div className="form-input">
+                        <Grid.Container gap={2.5} css={{
+                            mt: "4px",
+                            display: "grid",
+                            space: "",
+                            maxWidth: "400px",
+                            margin: "0 auto",
+                        }}>
+                            <Input
+                                size="md"
+                                placeholder="Name"
+                                css={{
+                                    borderRadius: "4px",
+                                    maxWidth: "400px",
+                                }}
+                            />
+                            <Grid>
+                                <Grid.Container gap={3} alignItems="center">
+                                    <Grid>
+                                        <Checkbox size="md" style={{
+                                            left: "-20px",
+                                            top: "5px"
+                                        }} ></Checkbox>
+                                        <Input
+                                            size="md"
+                                            placeholder="Company"
+                                            css={{
+                                                borderRadius: "4px",
+                                            }}
+                                        />
+                                    </Grid>
+                                </Grid.Container>
+                            </Grid>
+                            <Input
+                                size="md"
+                                placeholder="Phone"
+                                css={{
+                                    borderRadius: "4px",
+                                    maxWidth: "400px",
+                                }}
+                            />
+                            <Grid style={{
+                                border:"red"
+                            }}>
+                                <Textarea
+                                    size="md"
+                                    label=""
+                                    helperText="Please don't provide links"
+                                    placeholder="_"
+                                    css={{
+                                        maxWidth: "400px",
+                                        width: "400px",
+                                        // height: "50px",
+                                        top: "20px",
+                                        right: "20px",
+                                        maxHeight: "800",
+                                        // resize: "vertical"
+                                    }}
+                                />
+
+                            </Grid>
+                        </Grid.Container>
+
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            marginTop: "20px",
+                        }}>
+                            <Button color="primary" auto style={{ marginRight: "10px" }}>
+                                Send
+                            </Button>
+                            <Button color="error" auto style={{ marginLeft: "10px" }}>
+                                Cancel
+                            </Button>
+                        </div>
+
+
+
+                    </div>
+                </div>
+
+                <div className="contact-us">
+
+                </div>
+
+
+
+                <Footer/>
             </>
     )
 }
